74ffcc86d1d88caee60d309996a2eb66bf9b7797
michael.p.rosario <michael.p.rosario@gmail.com>
2024-07-14 12:35:45 +0000

    feat ( code of conduct ) adding content from old site

---
+++ src/organization-services.ts
@@ -0,0 +1,17 @@
+export class OrganizationInfo
+{
+    name: string = "";
+    conferenceEmail: string = "";
+    codeOfConductReportFormLink: string = "";
+}
+
+export class OrganizationServices
+{
+    getOrganizationInfo() : OrganizationInfo{
+        let record = new OrganizationInfo();
+        record.name = "Google Developer Group of Central Florida";
+        record.conferenceEmail = "organizers@devfestflorida.org";
+        record.codeOfConductReportFormLink = "https://fixme.com";
+        return record;
+    }
+}
