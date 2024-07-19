d3db18db1f761755bccb5852a36a9cffece0004c
michael.p.rosario <michael.p.rosario@gmail.com>
2024-07-19 03:10:49 +0000

    feat ( orlando front page ) copy from old site

--- src/pages/orlando/navigation.js
+++ src/pages/orlando/navigation.js
@@ -6,28 +6,26 @@ export const headerData = {
       text: 'Orlando',
       href: getPermalink('/orlando')
     },
-    {
-      text: 'Speakers',
-      href: getPermalink('/orlando/speakers')
-    },    
+    // {
+    //   text: 'Speakers',
+    //   href: getPermalink('/orlando/speakers')
+    // },    
     {
       text: 'Sponsors',
       href: getPermalink('/orlando/sponsors')
     },
-    {
-      text: 'Schedule',
-      href: getPermalink('/orlando/schedule')
-    },    
+    // {
+    //   text: 'Schedule',
+    //   href: getPermalink('/orlando/schedule')
+    // },    
     {
       text: 'Location',
       href: getPermalink('/orlando/location')
     },    
-    {
-      text: 'Blog',
-      href: getBlogPermalink()
-    },
-
-  
+    // {
+    //   text: 'Blog',
+    //   href: getBlogPermalink()
+    // },
 
   ],
 };
