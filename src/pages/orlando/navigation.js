cd781fb87ae219ea9788de91b5917ddf3fb68b18
Gil Creque <me@gilcreque.com>
2024-08-25 20:07:03 -0400

    Added schedule

--- src/pages/orlando/navigation.js
+++ src/pages/orlando/navigation.js
@@ -5,36 +5,38 @@ export const headerData = {
   links: [
     {
       text: 'Orlando',
-      href: getPermalink('/orlando')
+      href: getPermalink('/orlando'),
     },
     {
       text: 'Speakers',
-      href: getPermalink('/orlando/speakers')
-    },    
+      href: getPermalink('/orlando/speakers'),
+    },
     {
       text: 'Sponsors',
-      href: getPermalink('/orlando/sponsors')
+      href: getPermalink('/orlando/sponsors'),
     },
-    // {
-    //   text: 'Schedule',
-    //   href: getPermalink('/orlando/schedule')
-    // },    
+    {
+      text: 'Schedule',
+      href: getPermalink('/orlando/schedule'),
+    },
     {
       text: 'Location',
-      href: getPermalink('/orlando/location')
-    },    
+      href: getPermalink('/orlando/location'),
+    },
     {
       text: 'Blog',
-      href: getPermalink('/blog')
+      href: getPermalink('/blog'),
     },
-
-  
-
   ],
   actions: [
-    { variant: 'primary', text: 'Buy a Ticket!', href: 'https://www.eventbrite.com/e/935541117667?aff=oddtdtcreator', icon: 'tabler:square-rounded-arrow-right',         target:'_blank'
-    }
-  ]
+    {
+      variant: 'primary',
+      text: 'Buy a Ticket!',
+      href: 'https://www.eventbrite.com/e/935541117667?aff=oddtdtcreator',
+      icon: 'tabler:square-rounded-arrow-right',
+      target: '_blank',
+    },
+  ],
 };
 
 export const footerData = footer;
