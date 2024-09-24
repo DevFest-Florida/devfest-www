fae04050c4485150140026fbb66c450a17e570b7
Gil Creque <me@gilcreque.com>
2024-09-23 22:00:52 -0400

    Initial commit

--- src/pages/miami/navigation.js
+++ src/pages/miami/navigation.js
@@ -1,97 +1,33 @@
-import { getAsset, getBlogPermalink, getPermalink } from '../../utils/permalinks';
+import { footerData as footer } from '~/navigation';
+import { getBlogPermalink, getPermalink } from '../../utils/permalinks';
 
 export const headerData = {
   links: [
     {
       text: 'Miami',
-      href: getPermalink('/miami')
+      href: getPermalink('/miami'),
     },
     {
       text: 'Speakers',
-      href: getPermalink('/miami/speakers')
-    },    
+      href: getPermalink('/miami/speakers'),
+    },
     {
       text: 'Sponsors',
-      href: getPermalink('/miami/sponsors')
+      href: getPermalink('/miami/sponsors'),
     },
     {
       text: 'Schedule',
-      href: getPermalink('/miami/schedule')
-    },    
+      href: getPermalink('/miami/schedule'),
+    },
     {
       text: 'Location',
-      href: getPermalink('/miami/location')
-    },    
+      href: getPermalink('/miami/location'),
+    },
     {
       text: 'Blog',
-      href: getBlogPermalink()
+      href: getBlogPermalink(),
     },
-
-  
-
   ],
 };
 
-export const footerData = {
-  links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
-  ],
-  secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
-  ],
-  socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
-  ],
-  footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-  `,
-};
+export const footerData = footer;
